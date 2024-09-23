944aef2620724889ae6ae7bfeebc41fe102734c4
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-23 12:49:22 +0600

    create login antd design button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import "../styles/cartmodel.css";
+import "antd/dist/reset.css";
+import { Button } from "antd";
 
 const Login = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -67,7 +69,7 @@ const Login = () => {
                           {...register("email")}
                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="name@company.com"
-                          required=""
+                          required
                         />
                       </div>
                       <div>
@@ -80,16 +82,19 @@ const Login = () => {
                           {...register("password")}
                           placeholder="••••••••"
                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                          required=""
+                          required
                         />
                       </div>
 
-                      <button
-                        type="submit"
-                        className="w-full text-white bg-primary-green hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                      <Button
+                        htmlType="submit"
+                        type="primary"
+                        block
+                        size="large"
+                        shape="round"
                       >
                         Sign in
-                      </button>
+                      </Button>
                       <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                         Don’t have an account yet? please register
                       </p>
