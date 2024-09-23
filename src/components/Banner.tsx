336e819e6e3dd3a43a45a65f032c482e459513f0
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-24 01:59:05 +0600

    banner section complete

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import banner from "../assets/images/cover.png";
+import "../styles/button.css";
 
 const Banner = () => {
   return (
@@ -8,15 +10,15 @@ const Banner = () => {
         style={{
           width: "100%",
           height: "auto",
-          boxShadow: "0 4px 10px rgba(0, 0, 0, 0.5)",
+          boxShadow: "-4px 0 10px rgba(0, 0, 0, 0.5)",
           objectFit: "cover",
         }}
-        alt=""
+        alt="Banner"
       />
       <div
         style={{
           position: "absolute",
-          top: "70%",
+          top: "80%",
           left: "25%",
           transform: "translate(-50%, -50%)",
           color: "white",
@@ -24,17 +26,38 @@ const Banner = () => {
           textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
         }}
       >
-        <h1 className="uppercase Oswald font-bold text-6xl mb-0">Charging</h1>
-        <h1 className="uppercase Oswald font-bold text-6xl mb-0">Your LIFE</h1>
-        <h1 className="uppercase Oswald font-bold text-6xl">THROUGH FITNESS</h1>
+        <h1
+          className="uppercase Oswald font-bold text-6xl mb-0 animated-text2 "
+          style={{ animationDelay: "0.2s" }}
+        >
+          Charging
+        </h1>
+        <h1
+          className="uppercase Oswald font-bold text-6xl mb-0 animated-text2"
+          style={{ animationDelay: "0.4s" }}
+        >
+          Your LIFE
+        </h1>
+        <h1
+          className="uppercase Oswald font-bold text-6xl animated-text2"
+          style={{ animationDelay: "0.6s" }}
+        >
+          THROUGH FITNESS
+        </h1>
 
-        <div className="mt-16">
-          <h1 className="uppercase Oswald font-bold text-3xl mb-0">
+        <div className="mt-24">
+          <h1 className="uppercase Oswald font-bold text-3xl mb-0 animated-text">
             Join our exclusive training{" "}
           </h1>
-          <h1 className="uppercase Oswald font-bold text-3xl mb-0">
+          <h1 className="uppercase Oswald font-bold text-3xl mb-0 animated-text">
             session just for you
           </h1>
+        </div>
+
+        <div>
+          <Link to="/shop">
+            <button className="secondary-btn mt-28 uppercase"> shop now</button>
+          </Link>
         </div>
       </div>
     </div>
