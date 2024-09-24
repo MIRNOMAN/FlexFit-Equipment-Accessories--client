41c93532154d3e0268efb4efca848c698c09d943
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-24 22:33:23 +0600

    create freature name

--- src/components/ui/FeaturedProducts.tsx
+++ src/components/ui/FeaturedProducts.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import "../../styles/hrtag.css";
+
 const FeaturedProducts = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    // Set the hr to visible after the component mounts
+    const timer = setTimeout(() => {
+      setVisible(true);
+    }, 100); // Delay before showing the hr
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div>
-      <h1>this feature product</h1>
+    <div className="mx-auto max-w-6xl my-20  relative">
+      <h1 className="text-center font-oswald text-xl font-semibold tracking-widest">
+        EXCLUSIVE OUTLET DEALS
+      </h1>
+      <hr className={`mx-auto hr-animation ${visible ? "visible" : ""}`} />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Banner";
 import Categories from "../components/ui/Categories";
+import FeaturedProducts from "../components/ui/FeaturedProducts";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Categories />
+      <FeaturedProducts />
     </div>
   );
 };
