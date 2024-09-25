5d9af3f64acb784d77b719326ac8d90faa4f76ee
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-26 02:10:46 +0600

    create cards section

---
+++ src/assets/data/Productdata.js
@@ -0,0 +1,62 @@
+export const productsData = [
+  {
+    name: "Wireless Bluetooth Headphones",
+    price: 89.99,
+    stockQuantity: 150,
+    description:
+      "High-quality wireless headphones with noise cancellation and up to 30 hours of battery life.",
+    images: [
+      "https://i.ibb.co.com/7nR2gzx/Kettlebells.jpg",
+      "https://i.ibb.co.com/sChDG8S/Barbells.jpg",
+      "https://i.ibb.co.com/pxXKJ8H/Yoga-Mats.jpg",
+    ],
+    category: "Electronics",
+  },
+  {
+    name: "Smartphone Stand",
+    price: 19.99,
+    stockQuantity: 75,
+    description: "Adjustable smartphone stand for hands-free viewing.",
+    images: [
+      "https://i.ibb.co.com/sChDG8S/Barbells.jpg",
+      "https://i.ibb.co.com/pxXKJ8H/Yoga-Mats.jpg",
+    ],
+    category: "Accessories",
+  },
+  {
+    name: "Gaming Mouse",
+    price: 49.99,
+    stockQuantity: 200,
+    description: "Ergonomic gaming mouse with customizable RGB lighting.",
+    images: [
+      "https://i.ibb.co.com/7nR2gzx/Kettlebells.jpg",
+      "https://i.ibb.co.com/sChDG8S/Barbells.jpg",
+      "https://i.ibb.co.com/pxXKJ8H/Yoga-Mats.jpg",
+    ],
+    category: "Gaming",
+  },
+  {
+    name: "Laptop Backpack",
+    price: 59.99,
+    stockQuantity: 120,
+    description: "Durable laptop backpack with multiple compartments.",
+    images: [
+      "https://i.ibb.co.com/7nR2gzx/Kettlebells.jpg",
+      "https://i.ibb.co.com/sChDG8S/Barbells.jpg",
+      "https://i.ibb.co.com/pxXKJ8H/Yoga-Mats.jpg",
+    ],
+    category: "Bags",
+  },
+  {
+    name: "Bluetooth Speaker",
+    price: 39.99,
+    stockQuantity: 90,
+    description: "Portable Bluetooth speaker with high-quality sound.",
+    images: [
+      "https://i.ibb.co.com/7nR2gzx/Kettlebells.jpg",
+      "https://i.ibb.co.com/sChDG8S/Barbells.jpg",
+      "https://i.ibb.co.com/pxXKJ8H/Yoga-Mats.jpg",
+    ],
+    category: "Electronics",
+  },
+];
---
+++ src/types/productcartType.tsx
@@ -0,0 +1,8 @@
+export type Product = {
+  id: number;
+  price: number;
+  stockQuantity: number;
+  description: string;
+  images: string[];
+  category: string;
+};
