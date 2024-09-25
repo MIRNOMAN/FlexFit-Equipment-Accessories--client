bcdd6b4ba5638cbeae65550c85c8e1dce65036e4
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-25 23:36:43 +0600

    create footer successfully

--- src/share/Footer.tsx
+++ src/share/Footer.tsx
@@ -2,10 +2,10 @@ import logo from "../assets/icons/navber_logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900  px-8 text-gray-400 py-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start">
+    <footer className="bg-gray-900 px-8 text-gray-400 py-8">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {/* Logo Section */}
-        <div className="mb-6 md:mb-0">
+        <div className="mb-6">
           <img
             src={logo}
             alt="Company Logo"
@@ -17,26 +17,29 @@ const Footer = () => {
         </div>
 
         {/* Contact Information */}
-        <div className="mb-6 md:mb-0">
+        <div className="mb-6">
           <h4 className="text-lg text-white font-semibold mb-4">Contact Us</h4>
           <p className="mb-2">Email: abdullahalnoman1509@gmail.com</p>
           <p className="mb-2">Phone: +8801798341509</p>
           <p className="mb-2">
-            Address: 123 shahid faruk road, jatrabari, Dhaka
+            Address: 123 Shahid Faruk Road, Jatrabari, Dhaka
           </p>
         </div>
 
         {/* Social Media Links */}
-        <div className="mb-6 md:mb-0">
+        <div className="mb-6">
           <h4 className="text-lg text-white font-semibold mb-4">Follow Us</h4>
-          <div className="flex space-x-4">
-            <a href="#" className="text-blue-500 h-4 hover:text-white">
+          <div className="grid grid-cols-2 gap-2">
+            <a
+              href="https://www.linkedin.com/in/abdullah-al-noman-b154692a3"
+              className="text-blue-500 hover:text-white"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
                 y="0px"
-                width="100"
-                height="100"
+                width="40"
+                height="40"
                 viewBox="0 0 48 48"
               >
                 <path
@@ -49,24 +52,75 @@ const Footer = () => {
                 ></path>
               </svg>
             </a>
-            <a href="#" className="text-blue-400 hover:text-white">
+            <a
+              href="https://github.com/MIRNOMAN"
+              className="text-blue-400 hover:text-white"
+            >
               <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="40"
+                height="40"
+                viewBox="0 0 48 48"
               >
-                <path d="M..." />
+                <path
+                  fill="#fff"
+                  d="M41,24c0,9.4-7.6,17-17,17S7,33.4,7,24S14.6,7,24,7S41,14.6,41,24z"
+                ></path>
+                <path
+                  fill="#455a64"
+                  d="M21 41v-5.5c0-.3.2-.5.5-.5s.5.2.5.5V41h2v-6.5c0-.3.2-.5.5-.5s.5.2.5.5V41h2v-5.5c0-.3.2-.5.5-.5s.5.2.5.5V41h1.8c.2-.3.2-.6.2-1.1V36c0-2.2-1.9-5.2-4.3-5.2h-2.5c-2.3 0-4.3 3.1-4.3 5.2v3.9c0 .4.1.8.2 1.1L21 41 21 41zM40.1 26.4C40.1 26.4 40.1 26.4 40.1 26.4c0 0-1.3-.4-2.4-.4 0 0-.1 0-.1 0-1.1 0-2.9.3-2.9.3-.1 0-.1 0-.1-.1 0-.1 0-.1.1-.1.1 0 2-.3 3.1-.3 1.1 0 2.4.4 2.5.4.1 0 .1.1.1.2C40.2 26.3 40.2 26.4 40.1 26.4zM39.8 27.2C39.8 27.2 39.8 27.2 39.8 27.2c0 0-1.4-.4-2.6-.4-.9 0-3 .2-3.1.2-.1 0-.1 0-.1-.1 0-.1 0-.1.1-.1.1 0 2.2-.2 3.1-.2 1.3 0 2.6.4 2.6.4.1 0 .1.1.1.2C39.9 27.1 39.9 27.2 39.8 27.2zM7.8 26.4c-.1 0-.1 0-.1-.1 0-.1 0-.1.1-.2.8-.2 2.4-.5 3.3-.5.8 0 3.5.2 3.6.2.1 0 .1.1.1.1 0 .1-.1.1-.1.1 0 0-2.7-.2-3.5-.2C10.1 26 8.6 26.2 7.8 26.4 7.8 26.4 7.8 26.4 7.8 26.4zM8.2 27.9c0 0-.1 0-.1-.1 0-.1 0-.1 0-.2.1 0 1.4-.8 2.9-1 1.3-.2 4 .1 4.2.1.1 0 .1.1.1.1 0 .1-.1.1-.1.1 0 0 0 0 0 0 0 0-2.8-.3-4.1-.1C9.6 27.1 8.2 27.9 8.2 27.9 8.2 27.9 8.2 27.9 8.2 27.9z"
+                ></path>
+                <path
+                  fill="#455a64"
+                  d="M14.2,23.5c0-4.4,4.6-8.5,10.3-8.5c5.7,0,10.3,4,10.3,8.5S31.5,31,24.5,31S14.2,27.9,14.2,23.5z"
+                ></path>
+                <path
+                  fill="#455a64"
+                  d="M28.6 16.3c0 0 1.7-2.3 4.8-2.3 1.2 1.2.4 4.8 0 5.8L28.6 16.3zM20.4 16.3c0 0-1.7-2.3-4.8-2.3-1.2 1.2-.4 4.8 0 5.8L20.4 16.3zM20.1 35.9c0 0-2.3 0-2.8 0-1.2 0-2.3-.5-2.8-1.5-.6-1.1-1.1-2.3-2.6-3.3-.3-.2-.1-.4.4-.4.5.1 1.4.2 2.1 1.1.7.9 1.5 2 2.8 2 1.3 0 2.7 0 3.5-.9L20.1 35.9z"
+                ></path>
+                <path
+                  fill="#00bcd4"
+                  d="M24,4C13,4,4,13,4,24s9,20,20,20s20-9,20-20S35,4,24,4z M24,40c-8.8,0-16-7.2-16-16S15.2,8,24,8 s16,7.2,16,16S32.8,40,24,40z"
+                ></path>
               </svg>
             </a>
-            <a href="#" className="text-pink-600 hover:text-white">
+            <a
+              href="https://www.facebook.com/mirr.noman"
+              className="text-pink-600 hover:text-white"
+            >
               <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="40"
+                height="40"
+                viewBox="0 0 48 48"
               >
-                <path d="M..." />
+                <path
+                  fill="#039be5"
+                  d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"
+                ></path>
+              </svg>
+            </a>
+            <a
+              href="https://x.com/mirnoman27139"
+              className="text-pink-600 hover:text-white"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="40"
+                height="40"
+                viewBox="0 0 50 50"
+              >
+                <path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
               </svg>
             </a>
           </div>
